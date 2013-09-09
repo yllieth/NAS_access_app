4b4a366d72e5d840983b93d3986afe0563c96e8e
Yllieth <sylvnimes@hotmail.com>
2013-09-09 20:14:27 +0200

    [CONFIG] host_address

--- src/com/NASAccessApp/ConfigActivity.java
+++ src/com/NASAccessApp/ConfigActivity.java
@@ -1,10 +1,17 @@
 package com.NASAccessApp;
 
-import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.preference.EditTextPreference;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceActivity;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.util.Log;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Gère la configuration de l'application.
@@ -28,41 +35,18 @@ public class ConfigActivity extends PreferenceActivity
 	// ##################################################################
 	
 	/**
-	 * Identifiant du champ permettant de définir l'adresse du NAS à observer.
-	 * Cette chaine est définie dans <code>res/strings</code>.
+	 * Afin de pouvoir mettre à jour dynamiquement le summary, on enregistre
+	 * la phrase initiale afin de pouvoir composer le summary comme suit :
+	 * - <phrase initiale> (<valeur actuelle>)
+	 *
+	 * > modifiée dans registerPreference()
+	 * > lue dans updateSummary
 	 */
-	private String pref_host_address_key;
-	
-	/** 
-	 * Elément du layout définit dans <code>res/layout/configuration.xml</code> 
-	 * permettant de définir l'adresse du NAS à observer.
-	 */
-	private EditTextPreference pref_host_address;
+	private Map<String, String> summaries = new HashMap <String, String>();
 
 	// ##################################################################
 	// ###                    FONCTIONS PUBLIQUES                     ###
 	// ##################################################################
-	
-	/** 
-	 * Fonction exécutée lors d'une modification de la configuration.
-	 *
-	 * - Modification de l'adresse du NAS à observer : 
-	 *		on modifie l'attribut <code>summary</code> du EditTextPreference
-	 *		pour y indiquer l'adresse que l'on vient de définir.
-	 * 
-	 * @important Cette fonction nécessite que la classe implémente OnSharedPreferenceChangeListener
-	 * @author Sylvain 08/07/2013
-	 */
-	public void onSharedPreferenceChanged(SharedPreferences configuration, 
-										  String key)
-	{
-		if (key.equals(pref_host_address_key)) {
-			pref_host_address.setSummary(
-				getString(R.string.pref_host_address_summary) + " (" +
-				configuration.getString(pref_host_address_key, "") + ")"
-			);
-		}
-	}
 	
 	/**
 	 * Fonction exécutée à la création de l'Activity.
@@ -78,9 +62,105 @@ public class ConfigActivity extends PreferenceActivity
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.layout.configuration);
 		
-		// initialisation
-		pref_host_address_key = getString(R.string.pref_host_address_key);
-		pref_host_address = (EditTextPreference) ConfigActivity.this.findPreference("conf_host_address");
+		registerPreference("pref_host_address", getString(R.string.pref_host_address_summary));
     }
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		// set up a listener whenever a key changes
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		
+		// unregister the listener whenever a key changes
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	}
+	
+	/**
+	 * Appelée lors de la création de l'activité pour initialiser le summary.
+	 * 
+	 * En principe, le summary est une phrase prédéfinie. Car, en principe, 
+	 * on n'affiche pas la valeur de la préférence dans le summary. Or c'est 
+	 * le cas ici !
+	 * 
+	 * @author Sylvain{09/09/2013}
+	 */
+	private void initSummary(Preference p) {
+		if (p instanceof PreferenceCategory) {
+			PreferenceCategory pCat = (PreferenceCategory) p;
+			for (int i = 0 ; i < pCat.getPreferenceCount() ; i++) {
+				initSummary(pCat.getPreference(i));
+			}
+		} else {
+			updateSummary(p);
+		}
+	}
+	
+	/**
+	 * Enregistre la préférence et permet surtout de gérer les summary 
+	 * qui vont devoir contenir la valeur de la préférence.
+	 * 
+	 * @example registerPreference("pref_host_address", getString(R.string.pref_host_address_summary))
+	 * Le premier paramètre est l'identifiant de la préférence
+	 * Le second parametre est la phrase initiale du summary
+	 * 
+	 * @author Sylvain{09/09/2013}
+	 */
+	private void registerPreference(String key, String baseSummary) {
+		Preference p = findPreference(key);
+		p.setSummary(baseSummary);
+		summaries.put(p.getKey(), baseSummary);
+		
+		initSummary(p);
+	}
+	
+	/**
+	 * Met à jour le summary d'une préférence pour indiquer la valeur
+	 * définie par l'utilisateur. Pour des raisons pratiques, cette
+	 * mise à jour n'est faite que lorsque la valeur de l'utilisateur
+	 * est une chaine de caractères qui a au moins 1 caractère.
+	 * 
+	 * @author Sylvain{09/09/2013}
+	 */
+	private void updateSummary(Preference p) {
+		String oldSummary = summaries.get(p.getKey());
+		String newSummary = "";
+		//Log.i("updateSummary", "AVANT : " + oldSummary);
+		
+		if (p instanceof ListPreference) {
+			ListPreference listPreference = (ListPreference) p;
+			newSummary = (String) listPreference.getEntry();
+		}
+		
+		if (p instanceof EditTextPreference) {
+			EditTextPreference editTextPreference = (EditTextPreference) p;
+			newSummary = editTextPreference.getText();
+		}
+
+		if (newSummary.length() > 0) {
+			newSummary = oldSummary + " (" + newSummary + ")";
+			//Log.i("updateSummary", "APRES : " + newSummary);
+			p.setSummary(newSummary);
+		}
+	}
+	
+	/** 
+	 * Fonction exécutée lors d'une modification de la configuration.
+	 *
+	 * - Modification de l'adresse du NAS à observer : 
+	 *		on modifie l'attribut <code>summary</code> du EditTextPreference
+	 *		pour y indiquer l'adresse que l'on vient de définir.
+	 * 
+	 * @important Cette fonction nécessite que la classe implémente OnSharedPreferenceChangeListener
+	 * @author Sylvain{08/07/2013}
+	 */
+	public void onSharedPreferenceChanged(SharedPreferences configuration, String key) {
+		initSummary(findPreference(key));
+	}
 }
 
